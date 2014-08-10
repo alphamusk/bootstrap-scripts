0c7db554d0983ec5cc541bec247bb2c148ee6ddb
AlphaMusk <alphamusk@networkpulse.com>
2014-08-09 21:04:28 -0600

    Initial

---
+++ getLastestGitCode.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Get lastest code
+# Version: 140621, 140809
+# Author: AlphaMusk.com
+
+num='0'
+pause='5'
+giturl='https://github.com/alphamusk/'
+dirpath="${1}"
+gitrepo="${2}"
+maxrun="${3}"
+codepath="${dirpath}/${gitrepo}"
+
+# Check if local repository was specified
+if [ "${1}" = '' ]
+then
+	echo "Error: directory path for code required!"
+	echo "$0 [directory path for code] [git repository name]"
+	exit;
+fi
+
+# Check if git url was specified
+if [ "${2}" = '' ]
+then
+	echo "Error: git repository name required!"
+	echo "$0 [git repository name]"
+	exit;
+fi
+
+# Check if a max run count was specified, defaults to 1 run
+if [ "${3}" = '' ] || [ "${3}" = 0 ]
+then
+	echo "Maximum run not specified, running ${0} once"
+	maxrun='1'
+fi
+
+# Change location to dirpath
+cd ${codepath} > /dev/null 2>&1
+if [ "$(echo $?)" != '0' ]
+then
+	echo "Error: git repository not cloned to ${codepath}"
+	echo "Run: cd ${dirpath} && git clone ${giturl}${gitrepo}"
+	exit;
+fi
+
+# Pull code from specified git repo
+while [ "${num}" != "${maxrun}" ]
+do
+	echo '';
+	echo '----------------------------------------------------------------------------------------------';
+	echo "Pulling git code from: ${giturl}${gitrepo}"
+	git pull ${giturl}${gitrepo}.git
+	echo '----------------------------------------------------------------------------------------------';
+	
+	# Increment run count
+	num=$((${num} + 1))
+
+	# If maxrun is greater than 1
+	if [ ${maxrun} != 0 ] && [ ${maxrun} != 1 ] && [ ${maxrun} != ${num} ]
+	then
+		echo "Run: ${num} of ${maxrun}";
+		echo ''
+		echo "Sleeping for ${pause} seconds";
+		sleep "${pause}";
+	else
+		# Last run
+		echo "Run: ${num} of ${maxrun}";
+		echo ''
+		exit;
+	fi
+done
