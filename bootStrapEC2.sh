9f99044f22862d3642ece92cae7d7351fef47a18
AlphaMusk <alphamusk@networkpulse.com>
2014-08-09 22:01:45 -0600

    Signed-off-by: AlphaMusk <alphamusk@networkpulse.com>

---
+++ bootStrapEC2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# BootStrapEC2
+# Version: 140809
+# Author: AlphaMusk.com
+
+## SETUP: Get Latest Git code
+# Create install directories
+mkdir -p /root/scripts
+cd /root/scripts
+
+# Download script
+wget https://github.com/alphamusk/bootstrap-scripts/blob/master/getLastestGitCode.sh
+chmod +x /root/scripts/getLastestGitCode.sh
+
+# Clone latest code for AppServer
+cd /var/www/html
+git clone https://github.com/alphamusk/mock-app-server
+
+# Create crontab for getting latest code
+codeCMD="/root/scripts/getLastestGitCode.sh /var/www/html https://github.com/alphamusk/mock-app-server > /dev/null 2>&1"
+job="*/15 * * * * $codeCMD"
+cat <(grep -i -v "$codeCMD" <(crontab -l)) <(echo "$job") | crontab -
+
+# Run script once to grab AppServer code
+/root/scripts/getLastestGitCode.sh /var/www/html https://github.com/alphamusk/mock-app-server
+
+## SETUP: Get AppServer
+cd 
