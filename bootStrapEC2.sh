d416ec04754b44c35dbb2d656e2f0c944afb182e
AlphaMusk <alphamusk@networkpulse.com>
2014-08-09 22:25:06 -0600

    Signed-off-by: AlphaMusk <alphamusk@networkpulse.com>

--- bootStrapEC2.sh
+++ bootStrapEC2.sh
@@ -16,15 +16,15 @@ rm -f getLastestGitCode.sh && wget https://github.com/alphamusk/bootstrap-script
 chmod +x /root/scripts/getLastestGitCode.sh
 
 # Clone latest code for AppServer
-cd /var/www/html && git clone https://github.com/alphamusk/mock-app-server
+mkdir -p /opt && cd /opt && git clone https://github.com/alphamusk/mock-app-server
 
 # Create crontab for getting latest code
-codeCMD="/root/scripts/getLastestGitCode.sh /var/www/html https://github.com/alphamusk/mock-app-server > /dev/null 2>&1"
+codeCMD="/root/scripts/getLastestGitCode.sh /opt https://github.com/alphamusk/mock-app-server > /dev/null 2>&1"
 job="*/15 * * * * $codeCMD"
 cat <(grep -i -v "$codeCMD" <(crontab -l)) <(echo "$job") | crontab -
 
 # Run script once to grab AppServer code
-/root/scripts/getLastestGitCode.sh /var/www/html https://github.com/alphamusk/mock-app-server
+/root/scripts/getLastestGitCode.sh /opt https://github.com/alphamusk/mock-app-server
 
 ## SETUP: Get AppServer
 cd 
